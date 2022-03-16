c4f4944efdb9332432b581fd91d6372f1acfcb5a
jojoma8 <jojo_ma@hotmail.co.uk>
2022-03-16 02:33:04 +0000

    first

---
+++ components/Content.js
@@ -0,0 +1,14 @@
+export const content = {
+  title: "FrontAid CMS with Next.js",
+  index: {
+    title: "Index Page",
+    content: "<p>Some <strong>index</strong> content...</p>",
+  },
+  python: [
+    {
+      title: "Import Excel Files",
+      exploreText:
+        "This island, only a 20-minute bangka ride from Coron town, has an imposing, mysterious skyline that wouldn’t be out of place in a King Kong film. Flying over Coron, you see that the fortresslike, jungle-clad interior is largely inaccessible terrain pockmarked with lakes, two of which, Kayangan Lake and Barracuda Lake, can be visited. The entire island is the ancestral domain of the Tagbanua indigenous group, who are primarily fishers and gatherers of the very lucrative balinsasayaw (birds' nests).",
+    },
+  ],
+};
---
+++ components/Sidebar.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Sidebar() {
+  return (
+    <div
+      className="fixed top-0 left-0 h-screen w-16 m-0
+        flex flex-col bg-stone-900 text-white shadow-lg"
+    >
+      <i></i>
+    </div>
+  );
+}
+
+export default Sidebar;
